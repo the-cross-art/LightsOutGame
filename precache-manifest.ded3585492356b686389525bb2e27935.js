93ba564807a46ff62691b5ac59f967675c20e561
Imran Nazir <63959831+the-cross-art@users.noreply.github.com>
2021-06-05 17:07:32 +0530

    Updates

---
+++ precache-manifest.ded3585492356b686389525bb2e27935.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83a4849c770729d244d8",
+    "url": "/LightsOutGame/static/css/main.eb9f50cd.chunk.css"
+  },
+  {
+    "revision": "83a4849c770729d244d8",
+    "url": "/LightsOutGame/static/js/main.83a4849c.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/LightsOutGame/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "90b537c835ba9e8e107b",
+    "url": "/LightsOutGame/static/js/runtime~main.90b537c8.js"
+  },
+  {
+    "revision": "4e4a3379bfab7d126e6972f548d60edd",
+    "url": "/LightsOutGame/index.html"
+  }
+];
